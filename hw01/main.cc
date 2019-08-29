94abebb385535c8fd47d7dc548935cb7e71695fd
wlemuspr <wlemuspr@my.hpu.edu>
2019-08-28 15:31:51 -1000

    first commit

---
+++ hw01/main.cc
@@ -0,0 +1,24 @@
+//
+//  main.c
+//  hw01
+//
+//  Created by wendy Lemus on 8/28/19.
+//  Copyright © 2019 wendy Lemus. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    int n = 10, y = 5, x = 0;
+    
+    
+    x= n*y;
+    
+    
+    printf("what is n and y? The equation is n * y = x\n");
+    
+    
+    printf("x=%d\n",x);
+    
+}
