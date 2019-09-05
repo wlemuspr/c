845d5f308e90fc5ddb9f5dfa3dc6cc348f62592f
wlemuspr <wlemuspr@my.hpu.edu>
2019-09-04 15:37:49 -1000

    add make

---
+++ Add/Add/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Add
+//
+//  Created by wendy Lemus on 9/4/19.
+//  Copyright © 2019 wendy Lemus. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
---
+++ hw01/multiply.h
@@ -0,0 +1,9 @@
+// these preprocessor directives are a guard against this header filed
+// being included twice
+#ifndef SQUARED_H
+#define SQUARED_H
+
+// this is the declaration of the squared function
+double squared(double x);
+
+#endif
